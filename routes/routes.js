8dff3f8baa96a7e37da4b7bd9a9f5361fb8595ba
Tuka <Artem.soglaev@mail.ru>
2021-10-19 16:26:47 +0300

    make data structure looks priter and now redirect work

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,16 @@ const router = require("express").Router()
 var path = require('path');
 
 router.get('/', function(req, res) {
-    console.log('1324')
     res.sendFile(path.join(path.dirname(__dirname))+ "/pages/newsPage/MainPage.html");
 });
+router.get('/documents',(req,res)=>{
+    res.sendFile(path.join(path.dirname(__dirname))+ "/pages/documents/documents.html");
+})
+router.get('/contacts',(req,res)=>{
+    res.sendFile(path.join(path.dirname(__dirname))+ "/pages/contacts/contacts.html");
+})
+router.get('/kval',(req,res)=>{
+    res.sendFile(path.join(path.dirname(__dirname))+ "/pages/kval/kval.html");
+})
 
 module.exports = router
