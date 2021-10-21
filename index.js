9215aeaf417c149cc544e3f2e21f18723583d929
Tuka <Artem.soglaev@mail.ru>
2021-10-21 14:07:21 +0300

    for pull

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var app = express();
-const parser = require("body-parser").json();
+const express = require('express');
+const app = express();
 var path = require('path');
 
-app.use(express.urlencoded(true)); 
-app.use(express.json());
-app.use(parser);
+
+app.use(express.urlencoded(true)); // Чтобы получать req.body (методом post)
+app.use(express.json())
 app.use('/',require("./routes/routes"))
 app.use(express.static(__dirname +'/public'))
-
 app.use("/api", require('./data/api'))
 
 app.listen(5000,function(){
--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,21 @@
 const btn1 = document.getElementById('btnEnter')
 
-btn1.innerHTML += "123423"
+
+
+btn1.addEventListener('click' ,e=>{
+    let login = document.getElementById('inputLogin').value
+    console.log(login)
+    //send({newsTxT : login,password:pwd,mail:mail,admin:false,teacher:false,junAdmin:false,middleAdmin:false})
+})
+
+const send = async(body) => {
+    let res = await fetch("/api", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    let data = await res.json();
+}
