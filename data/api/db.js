6907694813890783798d15e24e6aefdbc6fe0486
Tuka <Artem.soglaev@mail.ru>
2021-10-21 10:31:03 +0300

    bag fix

--- data/api/db.js
+++ data/api/db.js
@@ -3,7 +3,7 @@ function connectDB() {
     const MongoClient = require("mongodb").MongoClient;
     const user = "Tuka"
     const pwd = "23445667423Ar"
-    const uri = `mongodb://${user}:${pwd}@MainCluster.krjpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    const uri = `mongodb+srv://Tuka:${pwd}@cluster0.8azro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
     return new MongoClient(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true
